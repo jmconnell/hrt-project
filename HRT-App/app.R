13633cfa5aeb6cd7b4aef45c79c893c52d9f1249
jmconnell <juliamconnell@gmail.com>
2017-09-26 20:38:52 -0400

    Next revision

--- HRT-App/app.R
+++ HRT-App/app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
@@ -46,5 +37,4 @@ server <- function(input, output) {
 }
 
 # Run the application 
-shinyApp(ui = ui, server = server)
-
+shinyApp(ui, server)
