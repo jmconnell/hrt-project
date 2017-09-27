8225f875e2592bee6d3cac0965081168635f56fe
jmconnell <juliamconnell@gmail.com>
2017-09-26 22:58:02 -0400

    update

--- HRT-App/app.R
+++ HRT-App/app.R
@@ -3,22 +3,39 @@ library(leaflet)
 
 clinics <- read.csv("C:/Users/Cellar Door/Documents/Git/hrt-project/clinics.csv")
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
   titlePanel("HRT Project"),
-  sidebarPanel(),
+  sidebarPanel(
+    
+    #adjust this to be dynamic and adjust according to choices in data
+    checkboxGroupInput("doctype","Type of Medical Care",
+                       c("HRT"="hrt",
+                         "Informed Consent Only"="ic")
+                       )
+      
+  ),
   mainPanel(
     leafletOutput("mymap")
   )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output, session) {
+  
+  if(clinics$street2!="")
+    street2 <- paste(clinics$street2,"<br>")
+  else
+    street2 <- ""
+  
+  #fix this url issue
+  #url <- a(clinics$name, href=clinics$website, target="_blank")
+
+  
+  popup1 <- paste(clinics$name,"<br>",clinics$street1,"<br>",street2,clinics$city,", ",clinics$state,", ",clinics$ZIP,"<br>",clinics$phone, sep="")
   
   output$mymap <- renderLeaflet({
     leaflet(data=clinics) %>%
       addTiles() %>%
-      addCircleMarkers(~long,~lat, popup=~name)
+      addCircleMarkers(~long,~lat,popup=popup1)
   })
   
 }
